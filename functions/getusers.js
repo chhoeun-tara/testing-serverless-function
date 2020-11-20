11ce79f66fac8eccdf5d647a116b94aea4cb39e1
Chhoeun Tara <chhoeuntara2016@gmail.com>
2020-11-20 10:47:44 +0700

    updated

--- functions/getusers.js
+++ functions/getusers.js
@@ -3,7 +3,11 @@ const axios = require('axios');
 
 exports.handler = function (event, context, callback) {
 
-    const {API_URL, API_CLIENT_ID, API_CLIENT_SECRET} = process.env;
+    const { API_URL, API_CLIENT_ID, API_CLIENT_SECRET } = process.env;
+
+    // const API_URL = 'https://api.github.com/users';
+    // const API_CLIENT_ID = 'a79809603c35bc59cafa';
+    // const API_CLIENT_SECRET = '05be48fb6163396095674ade81fbebca88d2a160';
 
     const URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secrets=${API_CLIENT_SECRET}`;
 
